d9b08cbe919278578eeb99dc88dff4f0537b43a9
ninieves <ninive.olivaresf@gmail.com>
2020-01-22 23:42:40 -0300

    css
    se añadio el css y js, esta en proceso

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,21 @@ $(function () {
   $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   })
+
+  $(document).ready(function() {
+
+    $('.link-gallery').click(function(){
+      var descripcion = $(this).attr('title');
+      $('#caption').html(descripcion);
+        var img = $(this).find('img');
+        var src = img.attr('src')
+        $('#img01').attr('src', src);
+      $('#myModal').css('display','block');
+      $('.modal-backdrop').remove();
+    });
+  
+    $('.close').click(function(){
+      $('#myModal').css('display','none');
+    });
+    
+  });
